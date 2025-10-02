0ce9c9851b7cd144407d19291fee8729fbfd887d
Mohammad Ruhan Zahif Akhter Ghantiwala <amk1013361@student.hamk.fi>
2025-10-03 01:38:47 +0300

    Add files via upload

---
+++ printerApp.java
@@ -0,0 +1,16 @@
+public class printerApp
+{
+    public static void main(String args[]) throws Exception
+    {
+        System.out.println("Printer App");
+        String app1 = "This app is made by : ";
+        String Creator = "Ruhan";
+        System.out.println(app1+Creator);
+        int num1 = 5;
+        int num2 = 2;
+        System.out.println("The value of number1 is " + num1);
+        int multiplication = num1 * num2;
+        System.out.println(num1 + " * " + num2 +" = "+multiplication);
+
+    }
+}
